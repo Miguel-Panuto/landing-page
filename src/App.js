5ddcef5864445c33ae854045022fe1e73237c0ec
Miguel Panuto <miguel.panuto@live.com>
2020-04-12 17:51:22 -0300

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import banner from './assets/images/banner.png';
+import { FiArrowDown } from "react-icons/fi";
+import './App.css'
 
 function App() {
   return (
-    <div>
-
+    <div className="main-content">
+      <header>
+        <h1>LP</h1>
+      </header>
+      <main>
+        <div className="container-banner">
+          <div className="center">
+            <div className="form">
+              <h2>
+                Preencha o formulário e inicie a sua
+                caminhada para virar um webmaster!
+              </h2>
+            </div>{/*form*/}
+          </div>{/*center*/}
+        </div>{/*container-banner*/}
+      </main>
     </div>
   );
 }
