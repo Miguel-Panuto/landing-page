d1eb04884d7ee2acd3a5b55b22aecb3f7615d3d9
Miguel Panuto <miguel.panuto@live.com>
2020-04-13 12:49:16 -0300

    Updates

---
+++ precache-manifest.dc1707d97a30f7c5d71f458adca5de33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215b73d52cc3c63a635fa81208151261",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "e80840415c10e61d3f41",
+    "url": "/landing-page/static/js/2.7058651a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/landing-page/static/js/2.7058651a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fc8a2d443eeba7c539",
+    "url": "/landing-page/static/js/main.3c09b9de.chunk.js"
+  },
+  {
+    "revision": "4bc3ad3bdeb511110dae",
+    "url": "/landing-page/static/js/runtime-main.cb3269a4.js"
+  },
+  {
+    "revision": "dd67779183aabb75da5a057e71ebe317",
+    "url": "/landing-page/static/media/banner.dd677791.png"
+  }
+]);
